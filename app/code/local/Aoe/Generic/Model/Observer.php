545ab2ddb3ae9d5e2ef645de0171293643da37e9
Dmytro Zavalkin <dmytro.zavalkin@aoemedia.de>
2013-09-19 17:27:13 +0200

    Implemented MAGEINT-76: Replace Tivoka library with Junior on Magento side - fixed namespace autoloader implementation

---
+++ app/code/local/Aoe/Generic/Model/Observer.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author Dmytro Zavalkin <dmytro.zavalkin@aoemedia.de>
+ */
+
+class Aoe_Generic_Model_Observer
+{
+    /**
+     * Register SPL autoload function
+     */
+    public function addNamespaceAutoloader()
+    {
+        spl_autoload_register(function($class) {
+            if (strrpos($class, '\\') !== false) {
+                // namespaced class name
+                $class = str_replace('\\', '_', ltrim($class, '\\'));
+            }
+
+            return Varien_Autoload::instance()->autoload($class);
+        }, true, true);
+    }
+}
